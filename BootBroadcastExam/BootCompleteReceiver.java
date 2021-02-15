acc478ba0798b5c93f3fd69f1cb4d61df811598f
DESKTOP-9RAS77D\20929 <20191548@g.dongseo.ac.kr>
2021-02-15 16:54:08 +0900

    Android7일차 강의

---
+++ BootBroadcastExam/BootCompleteReceiver.java
@@ -0,0 +1,13 @@
+package com.example.bootbroadcastexam;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class BootCompleteReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "부팅완료", Toast.LENGTH_SHORT).show();
+    }
+}
